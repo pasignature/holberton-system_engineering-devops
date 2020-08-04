247eef164ad48f25ba34ec551d2f1ccb2dcb08ea
pasignature <pasignature@gmail.com>
2020-08-04 14:05:12 +0000

    first commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create puppet file named holberton in /tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
