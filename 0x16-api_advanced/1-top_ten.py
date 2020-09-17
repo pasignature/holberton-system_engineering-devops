6f8c905a09b86a03b9b053acae5c96e3f5e65d76
Andrew <pasignature@gmail.com>
2020-09-17 14:39:10 +0000

    creates subs

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+top ten hot posts
+"""
+import requests as r
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and prints the titles of the first 10 hot posts
+    """
+    headers = {"User-Agent": "Frocuts"}
+    endpoint = "http://reddit.com/r/{}/hot.json?limit=10"
+    subs = r.get(endpoint.format(subreddit), headers=headers)
+    if subs.status_code != 200:
+        print(None)
+        return 0
+    subs = subs.json()
+    if subs["kind"] == "Listing":
+        for data in subs["data"]["children"]:
+            print(data["data"]["title"])
+    else:
+        print(None)
